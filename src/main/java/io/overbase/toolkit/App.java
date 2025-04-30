bd4bcc58cfa1b7a635427e432e11708ef567bd84
Rostyslav Rava <rostyslav@ravasoft.com>
2025-04-30 22:04:46 +0300

    Initial commit

---
+++ src/main/java/io/overbase/toolkit/App.java
@@ -0,0 +1,12 @@
+package io.overbase.toolkit;
+
+/**
+ * OverBase Toolkit Application Main Class
+ */
+public class App
+{
+    public static void main(String[] args)
+    {
+        System.out.println("OverBase Toolkit!");
+    }
+}
---
+++ src/test/java/io/overbase/toolkit/AppTest.java
@@ -0,0 +1,20 @@
+package io.overbase.toolkit;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for OverBase Toolkit Main App.
+ */
+public class AppTest
+{
+    /**
+     * 
+     */
+    @Test
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
