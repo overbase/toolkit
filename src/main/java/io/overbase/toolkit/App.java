ecd98e992fcb97f1e77cb7e7fc435bddbea2efcc
Rostyslav Rava <rostyslav@ravasoft.com>
2025-05-01 23:20:44 +0300

    Added clear screen support for ANSI terminal

--- src/main/java/io/overbase/toolkit/App.java
+++ src/main/java/io/overbase/toolkit/App.java
@@ -5,8 +5,13 @@ package io.overbase.toolkit;
  */
 public class App
 {
-    public static void main(String[] args)
-    {
+    public static void clearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+
+    public static void main(String[] args) {
+        clearScreen();
         System.out.println("OverBase Toolkit!");
         Environment.init();
         System.out.println("Current working directory: " + Environment.getCurrentDir());
